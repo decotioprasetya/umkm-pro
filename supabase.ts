0df8880c3b23bb15785ace0ceff76f45aa23a1a1
decotioprasetya <decotioprasetya6484@gmail.com>
2026-05-08 08:31:29 +0700

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,120 +1,15 @@
-const API_URL =
-  "https://script.google.com/macros/s/AKfycbx3RpUhQHoVzj46vcPUeLwGEIyfxGd9dvR598lboqfzLW5bkGa4ye978qWQx664a7IG/exec";
+const API =
+  "https://script.google.com/macros/s/AKfycbx9WDHSr0NGe--C1KUIksijTScPMEmJyZfOjrygi4wURKMd-Xa4SFAD6eTCFnyPUMLU/exec";
 
-export const supabase = {
+export async function api(data: any) {
 
-  from: (sheet: string) => ({
-
-    select: () => ({
-
-      order: async () => {
-
-        const res = await fetch(
-          `${API_URL}?sheet=${sheet}`
-        );
-
-        const data = await res.json();
-
-        return {
-          data,
-          error: null
-        };
-
-      },
-
-      limit: async () => {
-
-        const res = await fetch(
-          `${API_URL}?sheet=${sheet}`
-        );
-
-        const data = await res.json();
-
-        return {
-          data,
-          error: null
-        };
-
-      }
-
-    }),
-
-    insert: async (values: any[]) => {
-
-      await fetch(API_URL, {
-        method: "POST",
-        body: JSON.stringify({
-          sheet,
-          data: values
-        })
-      });
-
-      return {
-        data: null,
-        error: null
-      };
-
+  const res = await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     },
+    body: JSON.stringify(data)
+  });
 
-    update: async () => {
-
-      return {
-        data: null,
-        error: null
-      };
-
-    },
-
-    delete: async () => {
-
-      return {
-        data: null,
-        error: null
-      };
-
-    },
-
-    eq: () => ({
-
-      single: async () => {
-
-        return {
-          data: null,
-          error: null
-        };
-
-      }
-
-    })
-
-  }),
-
-  auth: {
-
-    onAuthStateChange: () => {
-
-      return {
-        data: {
-          subscription: {
-            unsubscribe: () => {}
-          }
-        }
-      };
-
-    },
-
-    getSession: async () => {
-
-      return {
-        data: {
-          session: null
-        }
-      };
-
-    }
-
-  }
-
-};
-
-export const isCloudReady = true;
+  return await res.json();
+}
